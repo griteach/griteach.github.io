875ca924fbc8deb033d9c04d486e1af09a1a391b
griteach <griteach22@gmail.com>
2022-09-01 00:43:03 +0900

    layout change

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "마크다운 익히기 #1",
+        "excerpt":"마크다운 정리      제목은 #1 ~ #6까지 사용한다.    import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App';   const root = ReactDOM.createRoot(   document.getElementById('root') as HTMLElement ); root.render(   &lt;React.StrictMode&gt;     &lt;App /&gt;   &lt;/React.StrictMode&gt; );  위의 코드 입력 때문에 다 갈아엎기로 했다. 참. 비효율적이야.  ","categories": ["TIL"],
+        "tags": ["markdown"],
+        "url": "/til/markdown-1/",
+        "teaser": null
+      }]
