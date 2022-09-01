6a6db9969e276ecd53e0cc63ab734a057d77e69d
griteach <griteach22@gmail.com>
2022-09-01 16:05:48 +0900

    markdown posting

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "마크다운 익히기 #1",
-        "excerpt":"마크다운 정리      제목은 #1 ~ #6까지 사용한다.    import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App';   const root = ReactDOM.createRoot(   document.getElementById('root') as HTMLElement ); root.render(   &lt;React.StrictMode&gt;     &lt;App /&gt;   &lt;/React.StrictMode&gt; );  위의 코드 입력 때문에 다 갈아엎기로 했다. 참. 비효율적이야.  ","categories": ["TIL"],
+        "excerpt":"NOTICE 🔅 Notice: 기본적인 노티스 ⚡️ Primary: 중요한 노티스 🚦 Info: 정보 노티스 ⛔️ Warning: 경고 노티스 ❌ Danger: Danger 노티스 ✅ Success: Success 노티스 하단의 중괄호를 이용해서 노티스 형태 알려주기. 아이콘은 직접 입력해 주는 방법밖에 없는 듯 하다. {: .notice--&lt;primary | info | warning | danger | success &gt;}...","categories": ["TIL"],
         "tags": ["markdown"],
         "url": "/til/markdown-1/",
         "teaser": null
