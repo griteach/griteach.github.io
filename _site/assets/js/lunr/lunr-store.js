aa5fb75c8b9ff0eb204782546560513fdc0cc161
griteach <griteach22@gmail.com>
2022-09-06 00:00:17 +0900

    change order of goal

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "목표",
-        "excerpt":"2022 목표 리액트 스터디 노마드코더 리액트 마스터 과정 마무리 코인트래커 (~22.9.5.) github : coin-tracker-react 트렐로 클론 넷플릭스 클론 노마드코더 무료 강좌 전체 마무리 트위터 클론 코딩 Next JS 시작하기 줌 클론코딩 실전형 리액트 Hooks 10개 Nest JS로 API만들기 초보자를 위한 리덕스 101 GraphQL로 영화 API 만들기 ‘지금 우리 학교는’ 리액트로...","categories": ["GOAL"],
+        "excerpt":"2022 목표 리액트 스터디 노마드코더 리액트 마스터 과정 마무리 코인트래커 (~22.9.5.) github : coin-tracker-react State management 트렐로 클론 Animations 넷플릭스 클론 노마드코더 무료 강좌 전체 마무리 실전형 리액트 Hooks 10개 초보자를 위한 리덕스 101 Next JS 시작하기 (SSR을 강화시켜주는 React Framework) 최종적으로 SSR이 SEO에서 중요한 역할을 하기에, Next JS를 알아야...","categories": ["GOAL"],
         "tags": ["goal","develop","plan"],
         "url": "/goal/life/",
         "teaser": null
@@ -21,7 +21,7 @@ var store = [{
     "excerpt":"NOTICE 🔅 Notice: 기본적인 노티스 ⚡️ Primary: 중요한 노티스 🚦 Info: 정보 노티스 ⛔️ Warning: 경고 노티스 ❌ Danger: Danger 노티스 ✅ Success: Success 노티스 하단의 중괄호를 이용해서 노티스 형태 알려주기. 아이콘은 직접 입력해 주는 방법밖에 없는 듯 하다. {: .notice--&lt;primary | info | warning | danger | success &gt;}...","url": "http://localhost:4000/_posts/2022-09-01-markdown-1/"
   },{
     "title": "목표",
-    "excerpt":"2022 목표 리액트 스터디 노마드코더 리액트 마스터 과정 마무리 코인트래커 (~22.9.5.) github : coin-tracker-react 트렐로 클론 넷플릭스 클론 노마드코더 무료 강좌 전체 마무리 트위터 클론 코딩 Next JS 시작하기 줌 클론코딩 실전형 리액트 Hooks 10개 Nest JS로 API만들기 초보자를 위한 리덕스 101 GraphQL로 영화 API 만들기 ‘지금 우리 학교는’ 리액트로...","url": "http://localhost:4000/_posts/2022-09-02-life/"
+    "excerpt":"2022 목표 리액트 스터디 노마드코더 리액트 마스터 과정 마무리 코인트래커 (~22.9.5.) github : coin-tracker-react State management 트렐로 클론 Animations 넷플릭스 클론 노마드코더 무료 강좌 전체 마무리 실전형 리액트 Hooks 10개 초보자를 위한 리덕스 101 Next JS 시작하기 (SSR을 강화시켜주는 React Framework) 최종적으로 SSR이 SEO에서 중요한 역할을 하기에, Next JS를 알아야...","url": "http://localhost:4000/_posts/2022-09-02-life/"
   },{
     "title": "React basic",
     "excerpt":"Router Router: 원하는 곳에 뿌려주는 용도. Nested Router 라우터 안의 라우터 웹사이트에서 탭을 사용할 때 도움이 된다. 섹션이 나눠졌을 때도 만들 수 있지. Outlet 활용: outlet을 활용하여 전체 Router에 필요한 컴포넌트 &amp; Route를 등록해두고 실제로 불러와야할 컴포넌트에서 &lt;Outlet /&gt;으로 삽입해준다. function Dashboard() { return ( &lt;div&gt; &lt;h1&gt;Dashboard&lt;/h1&gt; {/* This element...","url": "http://localhost:4000/_posts/2022-09-02-react-study/"
